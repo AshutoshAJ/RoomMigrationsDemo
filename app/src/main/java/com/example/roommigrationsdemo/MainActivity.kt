e35e8c47ae8f013620939b31b2d455ab4528d9e6
Ashutosh Jena <acool3103@gmail.com>
2023-01-24 22:45:08 +0530

    Migrated to Student DB Version 2

--- app/src/main/java/com/example/roommigrationsdemo/MainActivity.kt
+++ app/src/main/java/com/example/roommigrationsdemo/MainActivity.kt
@@ -20,8 +20,9 @@ class MainActivity : AppCompatActivity() {
         binding.button.setOnClickListener {
             lifecycleScope.launch {
                 binding.editText.text.let {
-                    dao.insertStudent(Student(0, it.toString()))
+                    dao.insertStudent(Student(0, it.toString(), binding.editTextEmail.text.toString()))
                     binding.editText.setText("")
+                    binding.editTextEmail.setText("")
                 }
             }
         }
